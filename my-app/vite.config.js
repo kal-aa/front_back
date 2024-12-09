e76e56ea7decd5b0f62812ac7bb8c13f73f6f02f
kal <sadkalshayee@gmail.com>
2024-12-09 08:39:14 -0800

    --amend

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/front-back/',
+  base: "/front_back/",
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+});
